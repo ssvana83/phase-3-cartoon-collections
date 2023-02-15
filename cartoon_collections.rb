ef07014d426bf720369a065159cd5c50e3d43ee7
Stacy Vana <98608384+ssvana83@users.noreply.github.com>
2023-02-15 08:33:01 -0500

    DONE

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,27 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+
+# method should accept an array of dwarf names and then print out each name, in number order, using puts
+def roll_call_dwarves(dwarf_names)
+  dwarf_names.each_with_index do |dwarf, index|
+    puts "#{index + 1}) #{dwarf}"
+    
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+# method should accept an array argument of planeteer calls that look like:
+# planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.map { |word| word.capitalize + "!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+# method should accept an array of calls and return true if any calls are longer than 4 characters
+def long_planeteer_calls(assorted_calls)
+  return false if assorted_calls.length > 4
+  true
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+# method should accept an array of strings then iterate through to find and return the first 
+# that is a type of cheese
+def find_the_cheese(cheese_array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese_array.find{|cheese| cheese_types.include?(cheese)}
 end
